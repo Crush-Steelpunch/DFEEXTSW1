8f122c66bc9beba127364c8dbdc89b5b4b172ec5
Leon L. Robinson <leon.robinson@qa.com>
2022-05-17 10:02:57 +0100

    update with join help

--- join-method.py
+++ join-method.py
@@ -1,3 +1,12 @@
+# Help for join 
+# |  join(self, iterable, /)
+# |      Concatenate any number of strings.
+# |     An iterable is something with a numbered index like a list or a tuple
+# |      The string whose method is called is inserted in between each given string.
+# |      The result is returned as a new string.
+# |
+# |      Example: '.'.join(['ab', 'pq', 'rs']) -> 'ab.pq.rs'
+
 list1 = ["cats", "dogs", "mice", "sheep", "ocelots", "frogs"]
 var1 = " and "
 
