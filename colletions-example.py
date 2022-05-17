d7f9d804c41dc5dec9777b1aa08eaa8db8105e82
Leon L. Robinson <leon.robinson@qa.com>
2022-05-17 10:07:19 +0100

    comment code for clarity

--- colletions-example.py
+++ colletions-example.py
@@ -1,6 +1,6 @@
 inputvar1 = input("Please type an author: ")
 
 booksdict = { "Margaret Atwood": ["The Handmaiden's Tale", "The Edible Woman", "Lady Oracle"] ,  "J.R.R. Tolkien": ["The hobbit", "Lord Of the Rings"], "Agatha Christie": ["Murder on the Orient Express", "Dial M for Murder", "Poitrot and the Big Mustache"] }
-valuefromkeyvar = booksdict[inputvar1]
+valuefromkeyvar = booksdict[inputvar1]  # Match ONE key and return the list in the value
 stingfromalistvar = ", ".join(sorted(valuefromkeyvar))
 print(stingfromalistvar)
